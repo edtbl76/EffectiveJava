3fb3ae41ca0544664bfd3ed97e860aef3a0473f3
Ed Mangini <me@emangini.com>
2020-06-27 14:20:34 -0400

    Item 45: Streams API

---
+++ src/main/java/LambdasAndStreams_6/UseStreamsJudiciously_45/IterationVsStreams/CartesianProduct/IterativeCartesianProduct.java
@@ -0,0 +1,19 @@
+package LambdasAndStreams_6.UseStreamsJudiciously_45.IterationVsStreams.CartesianProduct;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IterativeCartesianProduct {
+
+    private static List<Card> newDeck() {
+        List<Card> result = new ArrayList<>();
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                result.add(new Card(suit, rank));
+            }
+        }
+        return result;
+    }
+}
+
+
