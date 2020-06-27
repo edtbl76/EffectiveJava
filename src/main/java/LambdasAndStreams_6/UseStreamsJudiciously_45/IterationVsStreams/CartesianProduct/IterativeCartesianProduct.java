97ce0cb100bbfeafdfed5b5e76f4b433e073faa2
Ed Mangini <me@emangini.com>
2020-06-27 19:41:06 -0400

    Item 46: SideEffectFreeFunctions

--- src/main/java/LambdasAndStreams_6/UseStreamsJudiciously_45/IterationVsStreams/CartesianProduct/IterativeCartesianProduct.java
+++ src/main/java/LambdasAndStreams_6/UseStreamsJudiciously_45/IterationVsStreams/CartesianProduct/IterativeCartesianProduct.java
@@ -2,6 +2,7 @@ package LambdasAndStreams_6.UseStreamsJudiciously_45.IterationVsStreams.Cartesia
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class IterativeCartesianProduct {
 
